daef9d005388a890aa98802de62b430eea86a0ef
agnesnjoroge <git config --global user.email>
2023-11-09 13:22:47 +0300

    add: deploy workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
